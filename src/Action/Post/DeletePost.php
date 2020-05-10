5cca73df7294c0b9e36bf056838d121724161c4c
ludovicjj <jahanlud@gmail.com>
2020-05-11 00:45:51 +0200

    Return json with status code 200

--- src/Action/Post/DeletePost.php
+++ src/Action/Post/DeletePost.php
@@ -6,6 +6,7 @@ namespace App\Action\Post;
 
 use App\Domain\Command\DeletePostCommand;
 use League\Tactician\CommandBus;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -30,14 +31,15 @@ class DeletePost
     /**
      * @Route("/api/posts/{postId}", name="api_delete_post", methods={"DELETE"})
      * @param Request $request
+     * @return JsonResponse
      */
-    public function delete(Request $request)
+    public function delete(Request $request): JsonResponse
     {
         $postId = $request->attributes->get('postId');
         $command = new DeletePostCommand($postId);
 
         $this->commandBus->handle($command);
 
-        dd('in working...');
+        return new JsonResponse(['data' => []], 200);
     }
 }
