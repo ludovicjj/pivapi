9b8c2349c01dcea99c5c7abba0439d118d5d25c6
ludovicjj <jahanlud@gmail.com>
2020-05-11 00:37:34 +0200

    create exception PostNotFoundException and update ExceptionSubscriber

--- src/Action/Post/DeletePost.php
+++ src/Action/Post/DeletePost.php
@@ -4,16 +4,40 @@
 namespace App\Action\Post;
 
 
+use App\Domain\Command\DeletePostCommand;
+use League\Tactician\CommandBus;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class DeletePost
 {
+    /** @var SerializerInterface $serializer */
+    private $serializer;
+
+    /** @var CommandBus $commandBus */
+    private $commandBus;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        CommandBus $commandBus
+    )
+    {
+        $this->serializer = $serializer;
+        $this->commandBus = $commandBus;
+    }
+
     /**
      * @Route("/api/posts/{postId}", name="api_delete_post", methods={"DELETE"})
      * @param Request $request
      */
     public function delete(Request $request)
     {
+        $postId = $request->attributes->get('postId');
+        $command = new DeletePostCommand($postId);
+
+        $this->commandBus->handle($command);
+
+        dd('in working...');
     }
 }
