5e83516829d1bfe46768d0bbe5f8a37630184640
ludovicjj <jahanlud@gmail.com>
2020-05-14 13:44:29 +0200

    Update dev fixtures

--- src/Domain/DataFixtures/AppFixtures.php
+++ src/Domain/DataFixtures/AppFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\Domain\DataFixtures;
 
+use App\Domain\Core\Fixtures\Loader\Loader;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Fidry\AliceDataFixtures\Bridge\Doctrine\Persister\ObjectManagerPersister;
+use Fidry\AliceDataFixtures\Loader\PersisterLoader;
+use Psr\Log\NullLogger;
 
 class AppFixtures extends Fixture
 {
+    /** @var Loader $loader */
+    private $loader;
+
+    public function __construct(
+      Loader $loader
+    ) {
+        $this->loader = $loader;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $persister = new PersisterLoader(
+            $this->loader,
+            new ObjectManagerPersister($manager),
+            new NullLogger(),
+            []
+        );
 
-        $manager->flush();
+        $persister->load([__DIR__ . '/appFixture.yaml']);
     }
 }
