20b6dfa467decef0c649386cb4f45a898befc666
ludovicjj <jahanlud@gmail.com>
2020-05-18 16:31:42 +0200

    Rename var posts to paginatedPosts

--- src/Action/Post/SearchPost.php
+++ src/Action/Post/SearchPost.php
@@ -11,6 +11,7 @@ use App\Domain\Exceptions\UnknownQueryException;
 use App\Domain\Exceptions\UnknownDirectionException;
 use App\Domain\Exceptions\UnknownOrderException;
 use App\Domain\Request\Post\SearchPost\RequestLoader;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -52,10 +53,12 @@ class SearchPost
      */
     public function search(Request $request)
     {
-        $posts = $this->requestLoader->load($request);
-        $linkPaginator = $this->paginatorBuilder->build($posts->count())->getLink();
+        /** @var Paginator $paginatedPosts */
+        $paginatedPosts = $this->requestLoader->load($request);
 
-        $output = new OutputSearchResult(iterator_to_array($posts), $linkPaginator);
+        $linkPaginator = $this->paginatorBuilder->build($paginatedPosts->count())->getLink();
+
+        $output = new OutputSearchResult(iterator_to_array($paginatedPosts), $linkPaginator);
         $context = $this->parameterBagTransformer->transformQueryToContext($request->query);
 
         return new Response(
